17feaa1a2eb39874a1661534fc7c93d78ed509a2
Soufiane-coder <84711969+Soufiane-coder@users.noreply.github.com>
2023-07-12 00:24:29 +0100

    adding the dark mode to the header of the landing page

--- src/layout/landing-page/Header/Header.jsx
+++ src/layout/landing-page/Header/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentDisplayMode } from '../../../redux/display-mode/display-mode.selector';
 import Titre from '../Titre/Titre';
+import { displayModes } from '../../../enums/displayModes';
 import './Header.scss';
 
 
-const Header = () => (
-    <div className='header' >
-        <div className="background-white" />
-        <div className="triangle-white triangle-up-right" />
-        <div className="triangle-white triangle-buttom-left" />
-        <Titre />
-    </div>
-)
+const Header = ({ displayMode }) => {
+    const isDarkMode = displayMode === displayModes.dark;
+    return (
+        <div className={`header ${isDarkMode && 'dark-mode'}`} >
+            <div className={isDarkMode ? 'background-white bg-dark-p-color' : 'background-white'} />
+            <div className={`${isDarkMode && 'bg-dark-p-color'} triangle-white triangle-up-right`} />
+            <div className={`${isDarkMode && 'bg-dark-p-color'} triangle-white triangle-buttom-left`} />
+            <Titre />
+        </div>
+    )
+}
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+    displayMode: selectCurrentDisplayMode
+})
+
+
+
+export default connect(mapStateToProps)(Header);
